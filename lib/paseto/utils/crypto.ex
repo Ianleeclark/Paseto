c83c55394eb5ed610dd54cb592b143294ef765ed
Ian Clark <ian@ianleeclark.com>
2018-04-05 23:37:16 -0500

    Wrapping up work for the night

--- lib/paseto/utils/crypto.ex
+++ lib/paseto/utils/crypto.ex
@@ -1,35 +1,53 @@
 defmodule Paseto.Utils.Crypto do
 
   @spec aes_256_ctr(binary, String.t, binary) :: binary
-  def aes_256_ctr(<< key :: size(2048) >>, data, nonce) do
+  def aes_256_ctr(<< key :: 2048 >>, data, nonce) do
     {_, ciphertext} = :crypto.stream_init(:aes_ctr, key, nonce)
     |> :crypto.stream_encrypt(to_string(data))
 
     ciphertext
   end
 
+  @doc """
+  Performs a HMAC-SHA384
+  """
+  @spec hmac_sha384(String.t(), String.t()) :: String.t()
   def hmac_sha384(key, data) do
     :crypto.hmac(:sha384, key, data)
   end
+
+  @doc """
+  Performs a HMAC-SHA384 and limits the resultant size of the mac to `trim_bytes` total bytes.
+  """
+  @spec hmac_sha384(String.t(), String.t(), number) :: String.t()
   def hmac_sha384(key, data, trim_bytes) do
     :crypto.hmac(:sha384, key, data, trim_bytes)
   end
 
-  def hkdf(length, ikm, salt, info) do
-    hash_len = 32
-    prk = hmac_sha384(salt, ikm)
-    t = ""
-    okm = ""
+  @doc """
+  Key derivation function used to generate a key for the AES 256 counter encryption
 
-    entire_key = Enum.map(
-      0..Float.ceil(length / 32),
-      fn i ->
-        # TODO(ian): Fix this below it might be broken
-        t = hmac_sha384(prk, t <> info <> "[#{inspect(i+1)}]")
-        okm = okm <> t
+  More info can be found: https://tools.ietf.org/html/rfc5869
+  """
+  @spec hkdf(number, String.t(), String.t(), String.t())
+  def hkdf(length, ikm, salt, info) do
+    extract(salt, ikm)
+  end
+
+  defp extract(salt, ikm) do
+    :crypto.hmac(:sha384, salt, ikm)
+  end
+
+  defp expand() do
+    l = ???
+    n = Float.ceil(384 / )
+    Enum.into(
+      1..n,
+      "",
+      fn x ->
+        :crypto.hmac()
       end
     )
-
-    String.slice(entire_key, Range.new(0, length))
+    :crypto.hmac(:sha384, )
   end
 end
---
+++ test/paseto/crypto_test.exs
@@ -0,0 +1,9 @@
+defmodule PasetoTest.Utils.Crypto do
+  use ExUnit.Case
+  doctest Paseto.Utils.Crypto
+
+  alias Paseto.Utils.Crypto
+
+  describe "HKDF tests" do
+  end
+end
