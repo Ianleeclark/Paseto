28ecfc399db2702d6f5402cbf3d0d239f7dd95f2
Ian Clark <ian@ianleeclark.com>
2018-04-05 23:06:56 -0500

    Pre-Auth Encoding is complete

--- lib/paseto/utils/utils.ex
+++ lib/paseto/utils/utils.ex
@@ -5,20 +5,27 @@ defmodule Paseto.Utils.Utils do
 
   use Bitwise
 
+  @doc """
+  Handles padding multi-part messages before they're sent off to a cryptographic function.
+
+  NOTE: this is currently used in both v1 and v2 protocols.
+  NOTE: There's a 99% chance you're using this library incorrectly if you are calling this function.
+  """
   @spec pre_auth_encode([String.t]) :: String.t
   def pre_auth_encode(pieces) when is_list(pieces) do
-    convert(encode_little_endian(Enum.count(pieces))) <> Enum.into(
+    convert(le64(Enum.count(pieces))) <> Enum.into(
       pieces,
       <<>>,
       fn piece ->
-        convert(encode_little_endian(String.length(piece))) <> Base.encode16(piece)
+        convert(le64(String.length(piece))) <> Base.encode16(piece)
       end
     )
   end
 
-  # TODO(ian): Change type from any to charlist
-  @spec encode_little_endian(number) :: any
-  def encode_little_endian(chunk) do
+  @spec le64(number) :: any
+  defp le64(chunk) do
+    # Performs Little Endian 64 bit encoding
+
     Enum.into(
       0..7,
       <<>>,
@@ -33,10 +40,12 @@ defmodule Paseto.Utils.Utils do
     end)
   end
 
-  def convert(<< x :: 8 >>) do
+  @spec convert(binary) :: String.t
+  defp convert(<< x :: 8 >>) do
     x |> Integer.to_string(16) |> String.pad_leading(2, "0")
   end
-  def convert(<< x :: 8, rest :: binary >>) do
+  @spec convert(binary) :: String.t
+  defp convert(<< x :: 8, rest :: binary >>) do
     (x |> Integer.to_string(16) |> String.pad_leading(2, "0")) <> convert(rest)
   end
 end
