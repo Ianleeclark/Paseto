516784b3ca7c02d6ae866c1e876311cbdd8d41d0
Ian Clark <ian@ianleeclark.com>
2018-04-05 22:10:03 -0500

    Work on PAE

--- lib/paseto/utils/utils.ex
+++ lib/paseto/utils/utils.ex
@@ -5,39 +5,32 @@ defmodule Paseto.Utils.Utils do
 
   use Bitwise
 
-  @spec pre_auth_encode([]) :: String.t
-  def pre_auth_encode([]) do
-    "\x00\x00\x00\x00\x00\x00\x00\x00"
-  end
-  def pre_auth_encode(['']) do
-    "\x01\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00"
-  end
+  require Logger
+
   @spec pre_auth_encode([String.t]) :: String.t
   def pre_auth_encode(pieces) when is_list(pieces) do
-    output = encode_little_endian(Enum.count(pieces))
-    Enum.map(
+    encode_little_endian(Enum.count(pieces)) <> Enum.into(
       pieces,
+      <<>>,
       fn piece ->
-        output <> encode_little_endian(piece) <> piece
+        << x :: binary >> = piece
+        encode_little_endian(String.length(piece)) <> x
       end
     )
   end
 
-  @spec encode_little_endian(String.t) :: String.t
-  defp encode_little_endian(chunk) do
-    output = ""
-    Enum.map(
+  # TODO(ian): Change type from any to charlist
+  @spec encode_little_endian(number) :: any
+  def encode_little_endian(chunk) do
+    Enum.into(
       0..8,
-      fn i ->
-        if i == 7 do
-          n = chunk &&& 127
+      <<>>,
+      fn x ->
+        if x == 7 do
+          chunk = chunk &&& 127
         end
 
-        output = output <> (chunk &&& 255)
-        n = chunk >>> 8
-      end
-    )
-
-    output
+        << (((chunk >>> (x * 8)) &&& 255)) >>
+    end)
   end
 end
