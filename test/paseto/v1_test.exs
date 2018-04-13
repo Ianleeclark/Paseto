34d17f7c4662703de99e016f2f11e1e30370a8d1
Ian Clark <ian@ianleeclark.com>
2018-04-12 22:41:22 -0500

    Added footer tests, now for PSS padding

--- test/paseto/v1_test.exs
+++ test/paseto/v1_test.exs
@@ -44,9 +44,25 @@ defmodule PasetoTest.V1 do
     end
 
     test "Invalid PK fails to verify, with footer" do
+      message = "Test Message"
+      footer = "key-id:533434"
+      {_pk1, sk1} = :crypto.generate_key(:rsa, {2048, 65537})
+      {pk2, _sk2} = :crypto.generate_key(:rsa, {2048, 65537})
+      signed_token = V1.sign(message, sk1)
+      payload = String.replace(signed_token, "v1.public.", "")
+
+      assert V1.verify("v1.public.", payload, pk2) == {:error, "Failed to verify signature."}
     end
 
     test "Simple sign/verify, with footer" do
+      message = "Test Message"
+      footer = "key-id:533434"
+      {pk, sk} = :crypto.generate_key(:rsa, {2048, 65537})
+      signed_token = V1.sign(message, sk, footer)
+      payload = String.replace(signed_token, "v1.public.", "")
+      [_, _, payload, _] = String.split(signed_token, ".")
+
+      assert V1.verify("v1.public.", payload, pk, footer) == message
     end
   end
 end
