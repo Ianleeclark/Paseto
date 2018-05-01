b66784d0e1e71163108e175594467cfd50e54aeb
Ian Clark <ian@ianleeclark.com>
2018-04-30 19:46:31 -0500

    Fixing docs and removing some unused functions

--- lib/paseto.ex
+++ lib/paseto.ex
@@ -2,51 +2,23 @@ defmodule Paseto do
   @moduledoc """
   Main entry point for consumers. Will parse the provided payload and return a version
   (currently only v1 and v2 exist) struct.
+
+  Tokens are broken up into several components:
+  * version: v1 or v2 -- v2 suggested
+  * purpose: Local or Public -- Local -> Symmetric Encryption for payload & Public -> Asymmetric Encryption for payload
+  * payload: A signed or encrypted & b64 encoded string
+  * footer: An optional value, often used for storing keyIDs or other similar info.
   """
 
   alias Paseto.{Token, V1, V2}
 
   @doc """
-  Handles parsing a paseto token
-
-  ## Examples
-
-  iex> Paseto.parse("v1.local.dGVzdA==")
-  {:ok, %Paseto.V1{version: "v1", purpose: "local", payload: "test", footer: nil}}
-
   """
-  @spec parse(String.t()) :: {:ok, %Paseto.V1{}} | {:ok, %Paseto.V2{}} | {:error, String.t()}
-  def parse(token) do
-    case parse_token(token) do
-      {:ok, %{version: "v1"} = parsed_token} ->
-        {:ok, V1.from_token(parsed_token)}
-
-      {:ok, %{version: "v2"} = parsed_token} ->
-        {:ok, V2.from_token(parsed_token)}
-
-      {:ok, %{version: _}} ->
-        {:error, "Invalid token version. Only versions 1 & 2 are supported"}
-
-      {:error, _} = err ->
-        err
-    end
-  end
-
-  # TODO(ian): Add docstring and check out the typespec w/ exception
-  @spec parse!(String.t()) :: %Paseto.V1{} | %Paseto.V2{}
-  def parse!(payload) do
-    case parse(payload) do
-      {:ok, token} -> token
-      {:error, error_message} -> raise error_message
-    end
-  end
-
-  # TODO(ian): Add a docstring here explaining pieces.
-  @spec parse_token(String.t()) :: {:ok, %Token{}} | {:error, String.t()}
-  def parse_token(payload) do
-    case String.split(payload, ".") do
+  @spec parse_token(String.t(), binary()) :: {:ok, %Token{}} | {:error, String.t()}
+  defp parse_token(token, key) do
+    case String.split(token, ".") do
       [version, purpose, payload, footer] ->
-        case Base.decode64(payload) do
+        case Base.decode64(token) do
           {:ok, payload} ->
             {:ok,
              %Token{
@@ -77,6 +49,31 @@ defmodule Paseto do
 
       _ ->
         {:error, "Invalid token encountered during token parsing"}
+    end
+  end
+
+  @doc """
+  """
+  @spec generate_token(String.t(), String.t(), String.t(), String.t()) :: {:ok, String.t()} | {:error, String.t()}
+  def generate_token(version, purpose, payload, secret_key, footer \\ "") do
+    _generate_token(version, purpose, payload, secret_key, footer)
+  end
+
+  @spec _generate_token(String.t(), String.t(), binary, String.t(), String.t()) :: {:ok, String.t()} | {:error, String.t()}
+  defp _generate_token(version, "public", payload, secret_key, footer) do
+    case String.downcase(version) do
+      "v2" -> V2.sign(payload, secret_key, footer)
+      "v1" -> V1.sign(payload, secret_key, footer)
+      _ -> {:error, "Invalid version selected. Only v1 & v2 supported."}
+    end
+  end
+
+  @spec _generate_token(String.t(), String.t(), binary, String.t(), String.t()) :: {:ok, String.t()} | {:error, String.t()}
+  defp _generate_token(version, "local", payload, key, footer) do
+    case String.downcase(version) do
+      "v2" -> V2.encrypt(payload, key, footer)
+      "v1" -> V1.encrypt(payload, key, footer)
+      _ -> {:error, "Invalid version selected. Only v1 & v2 supported."}
     end
   end
 end
