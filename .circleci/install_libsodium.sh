1114225a323f350d215dee6e7dcfbed65633c5d0
Ian Clark <ian@ianleeclark.com>
2019-02-08 21:13:57 -0800

    Trying wget first

--- .circleci/install_libsodium.sh
+++ .circleci/install_libsodium.sh
@@ -1,10 +1,6 @@
 #!/usr/bin/env bash
 
 if [ ! -f /usr/local/lib/libsodium.so ]; then
-    if [ ! -f ./libsodium*.tar.gz ]; then
-        wget https://download.libsodium.org/libsodium/releases/old/libsodium-1.0.13.tar.gz
-    fi
-
     tar xf libsodium-1.0.13.tar.gz
     cd libsodium-1.0.13; ./configure; make; sudo make install
 else
