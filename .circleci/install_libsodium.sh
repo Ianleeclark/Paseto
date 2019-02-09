e07ef36fc3b1fb1b344b2c97d2943ae946c3aef1
Ian Clark <ian@ianleeclark.com>
2019-02-08 22:41:30 -0800

    Mv the so first

--- .circleci/install_libsodium.sh
+++ .circleci/install_libsodium.sh
@@ -1,5 +1,9 @@
 #!/usr/bin/env bash
 
+if [ -f libsodium.so]; then
+    sudo mv libsodium.so /usr/local/lib/libsodium.so
+fi
+
 if [ ! -f /usr/local/lib/libsodium.so ]; then
     mkdir libsodium
     tar xf .circleci/deps/libsodium.tar.gz -C ./libsodium/ --strip-components=1
