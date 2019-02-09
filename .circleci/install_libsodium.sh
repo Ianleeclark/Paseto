c6435f2e39083162ae7cba4592bd1544df7edbab
Ian Clark <ian@ianleeclark.com>
2019-02-08 21:21:49 -0800

    Updating bash script to do no version libsodium tarball

--- .circleci/install_libsodium.sh
+++ .circleci/install_libsodium.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 
 if [ ! -f /usr/local/lib/libsodium.so ]; then
-    tar xf libsodium-1.0.13.tar.gz
-    cd libsodium-1.0.13; ./configure; make; sudo make install
+    tar xf libsodium.tar.gz
+    cd libsodium; ./configure; make; sudo make install
 else
     echo "Libsodium cache hit"
 fi
