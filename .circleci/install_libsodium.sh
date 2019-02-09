cb8ac6a259b80ef71127b682cf877fac4f206137
Ian Clark <ian@ianleeclark.com>
2019-02-08 21:22:45 -0800

    Trying autogen before configure

--- .circleci/install_libsodium.sh
+++ .circleci/install_libsodium.sh
@@ -2,7 +2,7 @@
 
 if [ ! -f /usr/local/lib/libsodium.so ]; then
     tar xf libsodium.tar.gz
-    cd libsodium; ./configure; make; sudo make install
+    cd libsodium; ./autogen.sh; ./configure; make; sudo make install
 else
     echo "Libsodium cache hit"
 fi
