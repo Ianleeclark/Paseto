583edf1facb6dd068165d6d0e8e38e6a9621845f
Ian Clark <ian@ianleeclark.com>
2019-02-08 21:30:47 -0800

    strip dir

--- .circleci/install_libsodium.sh
+++ .circleci/install_libsodium.sh
@@ -2,7 +2,7 @@
 
 if [ ! -f /usr/local/lib/libsodium.so ]; then
     mkdir libsodium
-    tar -xf libsodium.tar.gz -C libsodium/
+    tar -xf libsodium.tar.gz -C libsodium/ --strip-components=1
     cd libsodium; ./autogen.sh; ./configure; make; sudo make install
 else
     echo "Libsodium cache hit"
