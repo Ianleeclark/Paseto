afdcfaaa573655ad1160f7455536ba11d0e0d228
Ian Clark <ian@ianleeclark.com>
2019-02-08 21:24:33 -0800

    Better absolute pathing

--- .circleci/install_libsodium.sh
+++ .circleci/install_libsodium.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 if [ ! -f /usr/local/lib/libsodium.so ]; then
-    tar xf libsodium.tar.gz -C libsodium
+    tar xf libsodium.tar.gz -C ./libsodium/
     cd libsodium; ./autogen.sh; ./configure; make; sudo make install
 else
     echo "Libsodium cache hit"
