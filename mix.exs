f16d5f53bea3104b3fa3041c6ba9d6586533ae24
Ian Clark <ian@ianleeclark.com>
2019-02-22 10:43:20 -0800

    Fixing version to not limit to 1.7 releases

--- mix.exs
+++ mix.exs
@@ -5,7 +5,7 @@ defmodule Paseto.MixProject do
     [
       app: :paseto,
       version: "1.2.0",
-      elixir: "~> 1.7.1",
+      elixir: "~> 1.7",
       start_permanent: Mix.env() == :prod,
       aliases: aliases(),
       deps: deps(),
