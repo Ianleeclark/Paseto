243a9a60ad6bbfa318ef4b48f1745da1f22549d6
Ian Lee Clark <ian@ianleeclark.com>
2022-04-23 01:36:26 +0200

    Finalizing changes

--- mix.exs
+++ mix.exs
@@ -5,7 +5,7 @@ defmodule Paseto.MixProject do
     [
       app: :paseto,
       version: "1.3.2",
-      elixir: "~> 1.9",
+      elixir: "~> 1.13",
       elixirc_paths: elixirc_paths(Mix.env()),
       start_permanent: Mix.env() == :prod,
       aliases: aliases(),
